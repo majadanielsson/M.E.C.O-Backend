ada4c0269ba68db2468297f8a6e4f8a4b1171500
Oscar Englöf <oscar.englof@hotmail.com>
2020-04-27 14:45:42 +0200

    get

--- routes/courseInstance.js
+++ routes/courseInstance.js
@@ -13,31 +13,20 @@ var jsonParser = bodyParser.json();
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// @route     GET api/courseInstance
+// @route     GET /reports
 // @desc      Test route
 // @access    Public
 router.get("/", urlencodedParser, async function (req, res, next) {
-  var courseCode = req.query.courseCode;
-  var courseCode = req.param;
   var courseID = req.query.courseID;
+  var responsible = req.user.name;
   //check if courseID was provided
-  if (courseID) {
+  if (responsible) {
     // Get course by ID in course collection
     try {
-      const course = await Course.findById(courseID);
-
-      res.json(course);
-    } catch (err) {
-      console.error(err.message);
-
-      res.status(500).send("Server Error");
-    }
-  } else if (courseCode) {
-    // Get all instances of a single course in the reports collection
-    try {
-      const report = await Report.find({ courseCode: courseCode });
-
-      res.json(report);
+      const courseInstances = Course.find({
+        instances: { $elemMatch: { responsible: responsible } },
+      });
+      res.json(courseInstances);
     } catch (err) {
       console.error(err.message);
 
@@ -77,7 +66,7 @@ router.post(
     var courseID = req.query.courseID;
     var instanceID = req.query.instanceID;
     var author = req.user.name;
-    console.log(author)
+    console.log(author);
     if (!errors.isEmpty()) {
       // There are errors. Render form again with sanitized values/errors messages.
       // Error messages can be returned in an array using `errors.array()`.
@@ -87,7 +76,8 @@ router.post(
       // Data from form is valid. Store in database
       console.log(req.body);
       const { questions } = req.body;
-      console.log(req.body)
+      console.log(questions);
+      console.log(req.body);
 
       try {
         const newReport = new Report({
