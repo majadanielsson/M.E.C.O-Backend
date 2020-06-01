408a1e2ca8419c9d60f1ab9540e7d0f407f5f975
christianwiren <christian@wiren.cc>
2020-06-01 13:14:04 +0200

    search cleanup

--- routes/search.js
+++ routes/search.js
@@ -1,26 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const Course = require("../models/Course");
-/* GET users listing. */
-router.get('/', async function (req, res, next) {
-  const q = (req.query.q.split(" ").length == 1) ? req.query.q.trim() : "\"" + req.query.q.trim().split(" ").join("\" \"") + "\"";
-  const page = (req.query.page) ? parseInt(req.query.page.trim()) : 0;
-  var totalCount = null;
 
-  if (page == 0) {
-    try {
+// @route     GET /search?q=
+// @desc      Searches courses and sorts them
+// @access    Public
+router.get('/', async function (req, res, next) {
+  try {
+    // Add quotes around words to require the words in results
+    const q = (req.query.q.split(" ").length == 1) ? req.query.q.trim() : "\"" + req.query.q.trim().split(" ").join("\" \"") + "\"";
+    const page = (req.query.page) ? parseInt(req.query.page) : 0;
+    var totalCount = null;
+
+    if (page == 0) {
       totalCount = await Course.countDocuments({
         $text: {
           $search: q
         }
       });
-      console.log(totalCount);
-    } catch (err) {
-      res.status(500).json("error counting");
     }
-  }
 
-  try {
     const response = await Course.find({
       $or: [{
         $text: {
@@ -31,12 +30,12 @@ router.get('/', async function (req, res, next) {
         _id: (q.length == 6) ? q.toUpperCase() : ""
       }
       ]
-    }).select("name nameEng extent extentUnit").sort({
+    }, { name: 1, nameEng: 1, extent: 1, extentUnit: 1 }).sort({
       date: -1
     }).skip(20 * page).limit(20);
     res.json({ data: response, total: totalCount });
   } catch (err) {
-    res.status(500).json("error");
+    res.status(500).json({ message: "Server error" });
   }
 });
 
